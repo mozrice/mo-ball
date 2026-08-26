5d76af85669df1eeed39e87fc056f9fe414adb9a
mozrice <solpowa@gmail.com>
2026-08-26 14:37:16 -0700

    Update MO Arcade games

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker: lets Mo Arcade open with no internet connection.
 // Bump CACHE when you change the site so phones pick up the new version.
-const CACHE = "mo-arcade-v209";
+const CACHE = "mo-arcade-v213";
 const FILES = [
   "./",
   "./index.html",
